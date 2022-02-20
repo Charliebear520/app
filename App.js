342112bf2af9ec0d319acdcfbff1c71ee637c28e
Charliebear <50406s97116@gmail.com>
2022-02-20 15:46:35 +0800

    add num and name

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>110919009</Text>
+      <Text>黃塏峻</Text>
       <StatusBar style="auto" />
     </View>
   );
