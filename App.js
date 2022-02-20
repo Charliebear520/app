c8d7aaa120ba826b08f9652d519f7d4d101f5dd8
Charliebear <50406s97116@gmail.com>
2022-02-20 20:40:47 +0800

    add image

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>110919009</Text>
-      <Text>黃塏峻</Text>
+      <Image style={styles.Image} source={require("./assets/桌布.jpg")}></Image>
+      <Text style={styles.Text1}>110919009</Text>
+      <Text style={styles.Text2}>黃 塏 峻</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Text1:{
+    color:'#c9ada7',
+    fontStyle:'normal',
+    fontSize:20,
+    letterSpacing:20,
+  },
+  Text2:{
+    color:'#ffbe0b',
+    fontWeight:'700',
+    fontSize:15,
+    letterSpacing:5,
+    marginTop:15,
+  },
+  Image: {
+    width: 500,
+    height: 500,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
